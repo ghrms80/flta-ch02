feb9d9483517f8978e213f02b0fd5925b5a5b8ae
jiho <ghrms80@gmail.com>
2023-01-11 20:21:03 +0900

    Putting the list into a card

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// import 'package:ch02_hello_flutter/recipe.dart';
+import 'recipe.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -49,11 +51,40 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       // 3
       body: SafeArea(
-        // TODO: Replace child: Container()
         // 4
-        child: Container(),
+        child: ListView.builder(
+          // 5
+          itemCount: Recipe.samples.length,
+          // 6
+          itemBuilder: (context, index) {
+            // 7
+            // TODO: Add GestureDetector
+            return buildRecipeCard(Recipe.samples[index]);
+          },
+        ),
       ),
     );
   }
-  // TODO: Add buildRecipeCard() here
+
+  Widget buildRecipeCard(Recipe recipe) {
+    // 1
+    return Card(
+      // 2
+      child: Column(
+        // 3
+        children: <Widget>[
+          // 4
+          Image(
+            image: AssetImage(
+              recipe.imageUrl,
+            ),
+          ),
+          // 5
+          Text(
+            recipe.label,
+          ),
+        ],
+      ),
+    );
+  }
 }
